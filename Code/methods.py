d1b95ab65c1b3f40d801981378c29ee341f2e207
Skai Nzeuton <snzeuton30@stuy.edu>
2023-05-18 10:22:15 -0400

    started working on generating M

--- Code/methods.py
+++ Code/methods.py
@@ -1,1 +1,7 @@
-print("Hello world")
+def generate_Ms(str, arr):
+    str_index = 0
+    for i in range(16):
+        M = 0
+        order = str_index % 4
+
+        M = M | (ord(str[str_index]) << (str_index % 4))
