63cb9cf049d6c19d4e1a098915a7a4ff5f93f74e
Skai Nzeuton <snzeuton30@stuy.edu>
2023-05-18 20:07:24 -0400

    trying to get function importing working

--- Code/methods.py
+++ Code/methods.py
@@ -1,7 +1,23 @@
-def generate_Ms(str, arr):
+def generate_Ms(str, Ms):
     str_index = 0
     for i in range(16):
-        M = 0
-        order = str_index % 4
+        if(i == 14):
+            Ms.append(0)
+        elif(i == 15):
+            Ms.append(8 * len(str))
+        else:
+            M = 0
 
-        M = M | (ord(str[str_index]) << (str_index % 4))
+            if(str_index < len(str)):
+                for j in range(4):
+                    M = M | (ord(str[str_index]) << (24 - 8*(str_index % 4)))
+                    str_index += 1
+
+                Ms.append(M)
+            elif(str_index == len(str) and i < 14):
+                Ms.append(1 << 31)
+            else:
+                Ms.append(M)
+            
+            
+        
