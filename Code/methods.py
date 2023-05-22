82d3783f6c1d0c5eb62dc0d0957198d9f9a5a75a
Jerry Teng <jteng30@stuy.edu>
2023-05-22 09:52:25 -0400

    Added initial vectors in decimal (and their equivalents in hex as a comment).

--- Code/methods.py
+++ Code/methods.py
@@ -1,5 +1,5 @@
 #print("Hello world")
-originalA = 19088743;
-originalB = 2309737967;
-originalC = 4275878552;
-originalD = 1985229328;
+originalA = 19088743; # in hex: 01234567
+originalB = 2309737967; # in hex: 89abcdef
+originalC = 4275878552; # in hex: fedcba98
+originalD = 1985229328; # in hex: 76543210
