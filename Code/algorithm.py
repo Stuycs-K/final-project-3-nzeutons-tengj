fb071f0285e7afc3164fd31e66bff130fbfe28b7
Skai-N <snzeuton30@stuy.edu>
2023-05-22 22:35:43 -0400

    algorithm.py loops over F 16 times and properly rotates inputs, Ms, Ks, and shifts

--- Code/algorithm.py
+++ Code/algorithm.py
@@ -13,21 +13,32 @@ message = f.read()
 
 Ms = generate_Ms(message)
 
-for M in Ms:
-    print(hex(M))
+# for M in Ms:
+#     print(hex(M))
 
 Ks = generate_Ks()
 
-for K in Ks:
-    print(hex(K))
+# for K in Ks:
+#     print(hex(K))
 
-print()
+# print()
 
-output = F(originalA, originalB, originalC, originalD, Ms[0], Ks[0], 7)
+# vectors = F(originalA, originalB, originalC, originalD, Ms[0], Ks[0], 7)
 
-for vector in output:
+# for vector in vectors:
+#     print(hex(vector))
+
+for i in range(16):
+    if(i == 0):
+        vectors = F(originalA, originalB, originalC, originalD, Ms[i], Ks[i], 22 - 5*(3 - (i % 4)))
+    else:
+        # print(hex(vectors[0]), hex(vectors[1]), hex(vectors[2]), hex(vectors[3]))
+        # print(hex(vectors[3]), hex(vectors[0]), hex(vectors[1]), hex(vectors[2]))
+        # print(str(i + 1) + ": " + str(22 - 5*(3 - (i % 4))))
+        # print()
+        vectors = F(vectors[3], vectors[0], vectors[1], vectors[2], Ms[i], Ks[i], 22 - 5*(3 - (i % 4)))
+
+# print()
+
+for vector in vectors:
     print(hex(vector))
-
-# for i in range(16):
-#     A
-#     F()
