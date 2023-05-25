14e58935febd989f592fb7c99152a121bca5ba85
Skai Nzeuton <snzeuton30@stuy.edu>
2023-05-24 20:21:49 -0400

    wrote base templates for c files

---
+++ Code/algorithm.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "methods.h"
+
+int main(int argc, char const *argv[])
+{
+    hello();
+}
---
+++ Code/methods.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "methods.h"
+
+void hello() {
+    printf("Hello world\n");
+}
